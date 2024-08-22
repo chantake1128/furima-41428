b32252c7c54bd2f05156682e2556d47b035c840b
chantake1128 <chantake1128@gmail.com>
2024-08-22 17:37:02 +0900

    レビュー修正（2回目）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-
   end
   
   def edit
-
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
