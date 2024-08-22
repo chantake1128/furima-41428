979c541717d85e338605a2b8d79e489514de874a
chantake1128 <chantake1128@gmail.com>
2024-08-22 10:28:31 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,20 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+    
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
     private
 
     def item_params
