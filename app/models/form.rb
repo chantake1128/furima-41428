0900bc31c9880c0bde26e684c8724a0d19cb22a7
chantake1128 <chantake1128@gmail.com>
2024-08-23 09:40:23 +0900

    Formオブジェクトの作成

---
+++ app/models/form.rb
@@ -0,0 +1,30 @@
+class Form
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :order_id
+ # building_nameは必須ではないためvalidatesは設定していません
+  validates :postal_code,           presence: true
+  validates :prefecture_id,         numericality: { other_than: 1, message: "can't be blank"}
+  validates :city,                  presence: true
+  validates :street_address,        presence: true
+
+  def save
+    return false unless valid?
+
+    ActiveRecord::Base.transaction do
+      order = Order.create!(user_id: user_id, item_id: item_id)
+      Delivery.create!(
+        postal_code:    postal_code,
+        prefecture_id:  prefecture_id,
+        city:           city,
+        street_address: street_address,
+        building_name:  building_name,
+        phone_number:   phone_number,
+        order_id:       order.id
+      )
+    end
+    true
+  rescue ActiveRecord::RecordInvalid
+    false
+  end
+end
